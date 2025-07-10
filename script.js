e44c07cb623ba4194919ddbac1afbf3a2de8b17f
Abdallah Tarek <14712023100934@stud.cu.edu.eg>
2025-07-10 14:27:58 +0300

    Added email validation

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+'use strict!'
 const form=document.querySelector('.SignForm');
+const email=document.querySelector('.email');
 const signPage=document.querySelector('.sign-up')
 const success=document.querySelector('.success-message');
+const emailPattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 form.addEventListener('submit',function(event){
     event.preventDefault();
+    if(emailPattern.test(email.value.trim())){
     signPage.style.display='none';
     success.style.display='flex';
+    signPage.classList.remove('active');
+    document.querySelector('.success-message p span').textContent=email.value;
+}
+else{
+signPage.classList.add('active');
+}
+})
+
+email.addEventListener('input',function(){
+    if(email.value=='')
+        signPage.classList.remove('active');
+ else if(!emailPattern.test(email.value.trim()))
+    signPage.classList.add('active');
+else signPage.classList.remove('active');
 })
